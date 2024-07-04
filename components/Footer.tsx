7f8c128a28d72fab1460eb18ec1c3ef13de0e51c
Leandro Vilanova <leandro.vilanova@univtec.com>
2024-07-04 16:41:44 -0300

    feat: Add Footer component for website attribution

---
+++ components/Footer.tsx
@@ -0,0 +1,17 @@
+export function Footer() {
+  return (
+    <footer className="bg-foreground py-8 text-background">
+      <div className="container text-right">
+        Made with 📚🧠🛠️ by{" "}
+        <a
+          href="https://leandro-vilanova.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="font-chackra font-bold"
+        >
+          LEV/D
+        </a>
+      </div>
+    </footer>
+  );
+}
