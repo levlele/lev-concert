2bbb38af80e2e3ed824ae72905fdc79aae92736c
Leandro Vilanova <leandro.vilanova@univtec.com>
2024-07-04 16:41:58 -0300

    chore: Update Filter component layout and styling

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -16,37 +16,39 @@ export function Filter({ initialOrderBy, onOrderByChange }: FilterProps) {
   };
 
   return (
-    <div className="container flex place-items-center bg-background py-8 font-chackra">
-      <h3 className="w-20">Order by</h3>
-      <ToggleGroup
-        type="single"
-        size="lg"
-        value={orderBy}
-        aria-label="Order by"
-        onValueChange={handleOrderBy}
-      >
-        <ToggleGroupItem
-          variant="outline"
-          value="date"
-          aria-label="Toggle Date"
+    <div className="sticky top-0 z-50 bg-foreground py-8 text-background">
+      <div className="container flex place-items-center font-chackra">
+        <h3 className="w-20">Order by</h3>
+        <ToggleGroup
+          type="single"
+          size="lg"
+          value={orderBy}
+          aria-label="Order by"
+          onValueChange={handleOrderBy}
         >
-          <span className="text-white">Date</span>
-        </ToggleGroupItem>
-        <ToggleGroupItem
-          variant="outline"
-          value="name"
-          aria-label="Toggle Name"
-        >
-          <span className="text-white">Name</span>
-        </ToggleGroupItem>
-        <ToggleGroupItem
-          variant="outline"
-          value="price"
-          aria-label="Toggle Price"
-        >
-          <span className="text-white">Price</span>
-        </ToggleGroupItem>
-      </ToggleGroup>
+          <ToggleGroupItem
+            variant="outline"
+            value="date"
+            aria-label="Toggle Date"
+          >
+            Date
+          </ToggleGroupItem>
+          <ToggleGroupItem
+            variant="outline"
+            value="name"
+            aria-label="Toggle Name"
+          >
+            Name
+          </ToggleGroupItem>
+          <ToggleGroupItem
+            variant="outline"
+            value="price"
+            aria-label="Toggle Price"
+          >
+            Price
+          </ToggleGroupItem>
+        </ToggleGroup>
+      </div>
     </div>
   );
 }
